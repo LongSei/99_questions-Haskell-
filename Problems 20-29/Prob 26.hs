8cf0035ee0193234852ed34432a02269e54ef00d
LongSei <74856548+LongSei@users.noreply.github.com>
2021-10-18 17:29:56 +0700

    Add files via upload

--- Problems 20-29/Prob 26.hs
+++ Problems 20-29/Prob 26.hs
@@ -1,4 +1,12 @@
+-- Solution 1 --
 import Data.List
 combinatios :: Int -> [a] -> [[a]]
 combinatios 0 _ = [[]]
 combinatios len xs = [y:ys | y:xs' <- tails xs, ys <- combinatios (len - 1) xs']
+
+-- Solution 2 --
+combination :: Int -> [a] -> [[a]]
+combination 0 xs = []
+combination 1 xs = map (:[]) xs
+combination n [] = []
+combination n (x:xs) = (map (x:) $ combination (n - 1) xs) ++ combination n xs

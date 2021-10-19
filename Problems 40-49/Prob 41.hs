a7ad184cf8c849a536843b182813f7ecdae040b1
LongSei <74856548+LongSei@users.noreply.github.com>
2021-10-19 10:39:59 +0700

    Add files via upload

---
+++ Problems 40-49/Prob 41.hs
@@ -0,0 +1,38 @@
+checknot :: Bool -> [Bool] -> Bool
+checknot boolen xs
+  | boolen `elem` xs = False
+  | otherwise = True
+
+isPrime :: Int -> Bool
+isPrime n
+  | n == 1 = False
+  | n == 0 = False
+  | n == 2 = True
+  | otherwise = checknot True $ map (\t -> n `mod` t == 0) [2.. ceiling $ sqrt $ fromIntegral n]
+
+primeR :: Int -> Int -> [Int]
+primeR left right
+    | right < left = primeR left right
+    | left == right = [left | isPrime left]
+    | otherwise = if isPrime left then left : primeR (left + 1) right else primeR (left + 1) right
+
+union :: [Int] -> [Int] -> [(Int, Int)]
+union xs ys = [(x, y) | x <- xs, y <- ys]
+
+checking :: Int -> [(Int, Int)] -> (Int, Int)
+checking _ [] = (0, 0)
+checking value (x:xs) = if uncurry (+) x == value then x else checking value xs
+
+goldbach :: Int -> (Int, Int)
+goldbach n
+  | n <= 2 || odd n = error "wrong input"
+  | otherwise = checking n $ union (primeR 2 (n - 1)) (primeR 2 (n - 1))
+
+goldbachlist :: Int -> Int -> [(Int, Int)]
+goldbachlist m n
+  | m > n = goldbachlist n m
+  | m == n = [goldbach n | even n && goldbach n /= (0, 0)]
+  | otherwise = if m > 2 && even m then goldbach m : goldbachlist (m + 1) n else goldbachlist (m + 1) n
+
+goldbachlist' :: Int -> Int -> Int -> [(Int, Int)]
+goldbachlist' left right under = filter (\t -> fst t >= under && snd t >= under) (goldbachlist left right)

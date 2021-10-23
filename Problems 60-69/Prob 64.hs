c525902046621315c3a4d9e162151796b7e7847c
LongSei <74856548+LongSei@users.noreply.github.com>
2021-10-23 17:38:06 +0700

    Add files via upload

---
+++ Problems 60-69/Prob 64.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
+import Data.Maybe
+import Data.List
+import Control.Monad
+import Data.Bits
+data Tree a = Empty | Branch a (Tree a) (Tree a) deriving (Show, Eq)
+leaf :: a -> Tree a
+leaf x = Branch x Empty Empty
+
+countNode :: Tree a -> Int 
+countNode Empty = 0
+countNode (Branch _ left right) = 1 + countNode left + countNode right
+
+solving :: Tree a -> Int -> Int -> Tree (a, (Int, Int))
+solving (Branch x Empty Empty) accleft level = Branch (x, (accleft + 1, level)) Empty Empty
+solving (Branch x left Empty) accleft level = Branch (x, (accleft + countNode left + 1, level)) (solving left accleft $ level + 1) Empty 
+solving (Branch x Empty right) accleft level = Branch (x, (accleft + 1, level)) Empty (solving right (accleft + 1) $ level + 1)
+solving (Branch x left right) accleft level = Branch (x, (accleft + countNode left + 1, level)) (solving left accleft $ level + 1) (solving right (accleft + countNode left + 1) $ level + 1)

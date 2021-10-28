00813b3a0d5cb71201b0efaf46e1e8328ed6a64a
LongSei <74856548+LongSei@users.noreply.github.com>
2021-10-28 11:40:42 +0700

    Add files via upload

--- Problems 10-19/Prob 15.hs
+++ Problems 10-19/Prob 15.hs
@@ -1,3 +1,8 @@
+-- C1: 
 repli :: [a] -> Int -> [a]
 repli [] n = []
 repli (x:xs) timer = (replicate timer x) ++ (repli xs timer)
+
+-- C2: 
+repli :: [a] -> Int -> [a]
+repli xs timer = foldl (\acc x -> acc ++ replicate timer x) [] xs

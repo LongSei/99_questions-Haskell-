dcd99c56c19659d774c4518da1ccd5806bdd8d1b
LongSei <74856548+LongSei@users.noreply.github.com>
2021-11-16 16:13:14 +0700

    Add files via upload

--- Problems 40-49/Prob 47.hs
+++ Problems 40-49/Prob 47.hs
@@ -1,5 +1,4 @@
-infixl 4 `or'`
-infixl 6 `and'`
+
 
 and',or',nor',nand',xor',impl',equ' :: Bool -> Bool -> Bool
 not' :: Bool -> Bool
@@ -13,6 +12,9 @@ nand' x y = not' $ and' x y
 xor' x y | x == y = False | otherwise = True
 impl' x = or' (not' x)
 equ' x y | x == y = True | otherwise = False
+
+infixl 4 `or'`
+infixl 6 `and'`
 
 table :: (Bool -> Bool -> Bool) -> IO ()
 table f = mapM_ putStrLn [show a ++ " " ++ show b ++ " " ++ show (f a b) | a <- xs, b <- ys] 
